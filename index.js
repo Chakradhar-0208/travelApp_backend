f0fc91fbf8521c204d7ee9c65e2185a1c866c8c1
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2025-09-16 19:31:03 +0530

    JWT Auth Middleware

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
-import testDB from "./routes/testDB.js"
 import dotenv from "dotenv";
 import cors from "cors";
-import testingRoutes from "./routes/testingRoute.js";
-import homeRoute from "./routes/homeRoute.js"
+import authRoutes from "./routes/authRoutes.js"
+import { requireRole } from "./middlewares/requireRole.js";
+import validateToken from "./middlewares/auth.js";
+import { rateLimiter } from "./middlewares/rateLimiting.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter)
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    dbName: "travelApp",
-  })
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.error("DB connection error:", err));
 
-app.use("/testDB",testDB);
-app.use("/testing", testingRoutes);
-app.use("/home", homeRoute)
+mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.error("DB Error: ", err))
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Travel App API");
 });
 
+// app.use("/auth", authRoutes)
+
+app.use("/test", validateToken, requireRole("admin"), (req, res) => res.send("Hola"))
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
--- routes/testingRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { testingGet, testingPut } from "../controller/testingController.js";
-
-const router = express.Router();
-
-router.get("/", testingGet);
-router.post("/", testingPut);
-
-export default router;
