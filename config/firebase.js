355744cea043e9bac31edbef5d168166a7c1b8b6
Dhanumjay Kiran Katakam <katakamkiran53@gmail.com>
2025-11-20 23:33:35 +0530

    errors resolved

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,22 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
+let serviceAccount;
+
+// If secret is provided by CI or production
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // Local fallback for development
+    const localKey = await import("./serviceAccountKey.json", {
+        assert: { type: "json" }
+    });
+    serviceAccount = localKey.default;
+}
+
+if (!admin.apps.length) {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+}
 
 export default admin;
